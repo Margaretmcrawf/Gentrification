f0ba10c38e42ba7190a0c80b95b4b0fd8c1c4de4
margaretmcrawf <margaretmcrawf@gmail.com>
2016-11-17 23:09:24 -0500

    markdown for report, changes in rent rates for subsidized

--- code/agent.py
+++ code/agent.py
@@ -6,12 +6,13 @@ import scipy.special as sps
 
 class Agent:
 
-	def __init__(self, loc, education=False, avg_income = 50000):
+	def __init__(self, loc, education=False, avg_income = 50000, sub_housing_rate = 0.50):
 		""" From NetLogo code: assign creativity (assume creatives have a little more income, but high creatives have much more)"""
 
 		self.creativity = np.random.choice([1,5,10], p=[0.8, 0.1, 0.1]) # 1 is low, 5 is medium, 10 is high
 		self.education = education # is the person educated? May be used to adjust income in future
 		self.loc = tuple(loc)
+		self.sub_housing_rate = sub_housing_rate
 
 		#determine initial income, which is a random value in a gamma dist. 
 		self.income = round(np.random.gamma(avg_income/10000)*10000)
@@ -31,7 +32,7 @@ class Agent:
 		neighbs = env.get_residential_neighbors(self.loc)
 
 		if self.is_subsidized: #rent is cheaper for subsidized agents.
-			rent = rent/2
+			rent *= self.sub_housing_rate
 
 		if rent > (self.income/4):
 			self.loc = tuple(neighbs[np.random.randint(len(neighbs))])
